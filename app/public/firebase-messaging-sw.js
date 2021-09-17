9a72ea49801f5bdd6cb837efb1122d707c30d1b8
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2021-09-17 09:02:29 +0200

    feat(wip). initializing firebase app in frontend

---
+++ app/public/firebase-messaging-sw.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/9.0.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging-compat.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+// Handle incoming messages. Called when:
+// - a message is received while the app has focus
+// - the user clicks on an app notification created by a service worker
+//   `messaging.onBackgroundMessage` handler.
+messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);
+  // ...
+});
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// Keep in mind that FCM will still show notification messages automatically 
+// and you should use data messages for custom notifications.
+// For more info see: 
+// https://firebase.google.com/docs/cloud-messaging/concept-options
+messaging.onBackgroundMessage(function (payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
+
+self.addEventListener('install', function () {
+  console.log('firebase-messaging-sw installed!')
+});
--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,25 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { getToken } from 'firebase/messaging';
+
 import './App.css';
 
 const App = () => {
+  const getFirebaseToken = useCallback(async () => {
+    getToken({ vapidKey: process.env.REACT_APP_PUBLIC_VAPID_KEY }).then((currentToken) => {
+      if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        // ...
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // ...
+    });
+  }, []);
+
   const registerDevice = useCallback(async () => {
     const payload = { deviceId: 'abc' };
     await fetch('http://localhost:4000/registerDevice', {
@@ -26,16 +44,24 @@ const App = () => {
     console.log('sendNotification');
   }, []);
 
+  useEffect(() => {
+    // Reister Firebase service worker
+    navigator.serviceWorker.register('firebase-messaging-sw.js');
+  }, []);
+
   return (
-  <div className="App">
-    <button onClick={registerDevice}>
-      Register my device
-    </button>
-    <button onClick={sendNotification}>
-      Send me a notification
-    </button>
-  </div>
-);
-  }
+    <div className="App">
+      <button onClick={getFirebaseToken}>
+        Get token
+      </button>
+      <button onClick={registerDevice}>
+        Register my device
+      </button>
+      <button onClick={sendNotification}>
+        Send me a notification
+      </button>
+    </div>
+  );
+}
 
 export default App;
